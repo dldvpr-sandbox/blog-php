baaf0d8b8dc645d15787281db27d5d89b4bf8be7
Ludovic <dormoy.dev@gmail.com>
2025-05-21 10:35:21 +0200

    first commit

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<pre>
+    <?php
+    print_r($_SERVER);
+    ?>
+</pre>
+
+<header>
+    <a href="/" class="logo">Blog</a>
+    <ul class="header-menu">
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/add-article.php' ? 'active' : '' ?>>
+            <a href="/add-article.php">Écrire un article</a>
+        </li>
+    </ul>
+</header>
