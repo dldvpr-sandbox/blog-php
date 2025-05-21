de59e1c7c086e52e4c2c0dfd860d72264f5a06ad
Ludovic <dormoy.dev@gmail.com>
2025-05-21 15:10:26 +0200

    afficher les articles sur la pages d’acceuil  et fake data

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+<?php
+$filename = __DIR__ . '/data/articles.json';
+$articles = [];
+
+if (file_exists($filename)) {
+    $articles = json_decode(file_get_contents($filename), true) ?? [];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <?php require_once 'includes/head.php' ?>
-    <link rel="stylesheet" href="public/css/index.css">
+    <link rel="stylesheet" href="/public/css/index.css">
     <title>Blog</title>
 </head>
 
@@ -11,9 +19,19 @@
 <div class="container">
     <?php require_once 'includes/header.php' ?>
     <div class="content">
+        <div class="articles-container">
+            <?php foreach ($articles as $a) : ?>
+                <div class="article block">
+                    <div class="overflow">
+                        <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+                    </div>
+                    <h2><?= $a['title'] ?></h2>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </div>
     <?php require_once 'includes/footer.php' ?>
 </div>
-</body>
 
+</body>
 </html>
