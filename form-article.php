630f2896c23e5a786754d4d9e468d6f80832e183
Ludovic <dormoy.dev@gmail.com>
2025-05-23 11:49:30 +0200

    can edit and create with same files

--- form-article.php
+++ form-article.php
@@ -2,7 +2,7 @@
 const ERROR_REQUIRED = 'Veuillez renseigner ce champ';
 const ERROR_TITLE_TOO_SHORT = 'Le titre est trop court';
 const ERROR_CONTENT_TOO_SHORT = 'L\'article est trop court';
-const ERROR_IMAGE_URL = 'L\'image doit être une URL valide';
+const ERROR_IMAGE_URL = 'L\'image doit être une url valide';
 $filename = __DIR__ . '/data/articles.json';
 $errors = [
     'title' => '',
@@ -10,9 +10,21 @@ $errors = [
     'category' => '',
     'content' => '',
 ];
+$category = '';
 
 if (file_exists($filename)) {
     $articles = json_decode(file_get_contents($filename), true) ?? [];
+}
+
+$_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$id = $_GET['id'] ?? '';
+if ($id) {
+    $articleIndex = array_search($id, array_column($articles, 'id'));
+    $article = $articles[$articleIndex];
+    $title = $article['title'];
+    $image = $article['image'];
+    $category = $article['category'];
+    $content = $article['content'];
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -54,13 +66,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     if (empty(array_filter($errors, fn ($e) => $e !== ''))) {
-        $articles = [...$articles, [
-            'title' => $title,
-            'image' => $image,
-            'category' => $category,
-            'content' => $content,
-            'id' => time()
-        ]];
+        if ($id) {
+            $articles[$articleIndex]['title'] = $title;
+            $articles[$articleIndex]['image'] = $image;
+            $articles[$articleIndex]['category'] = $category;
+            $articles[$articleIndex]['content'] = $content;
+        } else {
+            $articles = [...$articles, [
+                'title' => $title,
+                'image' => $image,
+                'category' => $category,
+                'content' => $content,
+                'id' => time()
+            ]];
+        }
         file_put_contents($filename, json_encode($articles));
         header('Location: /');
     }
@@ -70,11 +89,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <?php require_once 'includes/head.php' ?>
-    <link rel="stylesheet" href="/public/css/add-article.css">
+    <link rel="stylesheet" href="/public/css/form-article.css">
 
-    <title>Créer un article</title>
+    <title><?= $id ? 'Modifier' : 'Créer' ?> un article</title>
 </head>
 
 <body>
@@ -82,18 +102,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <?php require_once 'includes/header.php' ?>
     <div class="content">
         <div class="block p-20 form-container">
-            <h1>Ecrire un article</h1>
-            <form action="/add-article.php" method="post">
+            <h1><?= $id ? 'Modifier' : 'Écrire' ?> un article</h1>
+            <form action="/form-article.php<?= $id ? "?id=$id" : '' ?>" , method="post">
                 <div class="form-control">
                     <label for="title">Titre</label>
-                    <input type="text" name="title" id="title" value=<?= $title ?? '' ?>>
+                    <input type="text" name="title" id="title" value="<?= $title ?? '' ?>">
                     <?php if ($errors['title']) : ?>
                         <p class="text-danger"><?= $errors['title'] ?></p>
                     <?php endif; ?>
                 </div>
                 <div class="form-control">
                     <label for="image">Image</label>
-                    <input type="text" name="image" id="image" value=<?= $image ?? '' ?>>
+                    <input type="text" name="image" id="image" value="<?= $image ?? '' ?>">
                     <?php if ($errors['image']) : ?>
                         <p class="text-danger"><?= $errors['image'] ?></p>
                     <?php endif; ?>
@@ -101,9 +121,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <div class="form-control">
                     <label for="category">Catégorie</label>
                     <select name="category" id="category">
-                        <option value="technologie">Technologie</option>
-                        <option value="nature">Nature</option>
-                        <option value="politique">Politique</option>
+                        <option <?= !$category || $category === 'technologie' ? 'selected' : '' ?> value="technologie">Technologie</option>
+                        <option <?= $category === 'nature' ? 'selected' : '' ?> value="nature">Nature</option>
+                        <option <?= $category === 'politique' ? 'selected' : '' ?> value="politique">Politique</option>
                     </select>
                     <?php if ($errors['category']) : ?>
                         <p class="text-danger"><?= $errors['category'] ?></p>
@@ -111,14 +131,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </div>
                 <div class="form-control">
                     <label for="content">Contenu</label>
-                    <textarea name="content" id="content"></textarea>
+                    <textarea name="content" id="content"><?= $content ?? '' ?></textarea>
                     <?php if ($errors['content']) : ?>
                         <p class="text-danger"><?= $errors['content'] ?></p>
                     <?php endif; ?>
                 </div>
                 <div class="form-actions">
-                    <button class="btn btn-secondary" type="button">Annuler</button>
-                    <button class="btn btn-primary" type="submit">Sauvegarder</button>
+                    <a href="/" class="btn btn-secondary" type="button">Annuler</a>
+                    <button class="btn btn-primary" type="submit"><?= $id ? 'Modifier' : 'Sauvegarder' ?></button>
                 </div>
             </form>
         </div>
@@ -127,4 +147,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </div>
 
 </body>
+
 </html>
