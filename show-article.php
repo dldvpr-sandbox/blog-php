a04e8f0d39b57863a4eeb09b7e5fec123d602963
Ludovic <dormoy.dev@gmail.com>
2025-05-26 14:41:10 +0200

    can crud with DB and PDO database refacto

--- show-article.php
+++ show-article.php
@@ -1,16 +1,12 @@
 <?php
-
-$pdo = require_once 'database.php';
-$statement = $pdo->prepare('SELECT * FROM article WHERE id = :id');
+$articleDB = require_once __DIR__ . '/database/models/ArticleDB.php';
 $_GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $id = $_GET['id'] ?? '';
 
 if (!$id) {
     header('Location: /');
 } else {
-    $statement->bindValue(':id', $id);
-    $statement->execute();
-    $article = $statement->fetch();
+    $article = $articleDB->fetchOne($id);
 }
 ?>
 
