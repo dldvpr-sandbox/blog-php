2dbe34a01a6d920fe40ddeefa550360d60ad53b9
Ludovic <dormoy.dev@gmail.com>
2025-06-03 15:50:10 +0200

    mise en page basique auth et profil

--- includes/header.php
+++ includes/header.php
@@ -4,5 +4,16 @@
         <li class=<?= $_SERVER['REQUEST_URI'] === '/form-article.php' ? 'active' : '' ?>>
             <a href="/form-article.php">Écrire un article</a>
         </li>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-register.php' ? 'active' : '' ?>>
+            <a href="/auth-register.php">Inscription</a>
+        </li>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/auth-login.php' ? 'active' : '' ?>>
+            <a href="/auth-login.php">Connexion</a>
+        </li>
+            <a href="/auth-logout.php">Déconnexion</a>
+        </li>
+        <li class=<?= $_SERVER['REQUEST_URI'] === '/profil.php' ? 'active' : '' ?>>
+            <a href="/profil.php">Mon profil</a>
+        </li>
     </ul>
 </header>
