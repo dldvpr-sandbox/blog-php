f89c677bfb6ee84f8b714de99540c1f94e205c5b
Ludovic <dormoy.dev@gmail.com>
2025-06-03 22:48:31 +0200

    inscription

--- form-article.php
+++ form-article.php
@@ -90,7 +90,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <head>
     <?php require_once 'includes/head.php' ?>
-    <link rel="stylesheet" href="/public/css/form-article.css">
     <title><?= $id ? 'Modifier' : 'Créer' ?> un article</title>
 </head>
 
